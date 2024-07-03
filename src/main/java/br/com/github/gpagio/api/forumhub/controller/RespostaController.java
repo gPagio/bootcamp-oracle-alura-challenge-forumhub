6cb803f943b84a87e16ce5e4cec5b11ba63e8cb9
gPagio <guilhermepagio.contato@gmail.com>
2024-07-03 02:38:02 -0300

    docs: finaliza documentacao swagger

--- src/main/java/br/com/github/gpagio/api/forumhub/controller/RespostaController.java
+++ src/main/java/br/com/github/gpagio/api/forumhub/controller/RespostaController.java
@@ -2,7 +2,11 @@ package br.com.github.gpagio.api.forumhub.controller;
 
 
 import br.com.github.gpagio.api.forumhub.domain.resposta.*;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -27,6 +31,10 @@ public class RespostaController {
     private RespostaService respostaService;
 
     @GetMapping("/{idTopico}")
+    @Operation(summary = "Listar Respostas de Tópicos Específicos", description = "Lista todos as respostas de tópicos específicos por ID. A ordenação padrão é pelo ID da resposta em páginas de 10 itens.")
+    @Parameter(in = ParameterIn.QUERY, description = "Número da página para exibir. Primeira página é 0.", name = "page", content = @Content(schema = @Schema(type = "integer", defaultValue = "0")))
+    @Parameter(in = ParameterIn.QUERY, description = "Quantidade de itens exibidos. Valor padrão é 10.", name = "size", content = @Content(schema = @Schema(type = "integer", defaultValue = "10")))
+    @Parameter(in = ParameterIn.QUERY, description = "Critério de ordenação dos itens. Campo padrão é ID.", name = "sort", content = @Content(schema = @Schema(type = "string", defaultValue = "id")))
     public ResponseEntity listarRespostasTopicoEspecifico(@PathVariable Long idTopico, @Parameter(hidden = true) @PageableDefault(size = 10, sort = "id") Pageable paginacao){
         var page = respostaRepository.findAllByTopicoId(paginacao, idTopico).map(DadosDetalhamentoResposta::new);
         return ResponseEntity.ok(page);
@@ -34,6 +42,7 @@ public class RespostaController {
 
     @PostMapping("/{idTopico}")
     @Transactional
+    @Operation(summary = "Postar Resposta", description = "Realiza a publicação de uma resposta em um tópico específico no fórum.")
     public ResponseEntity postar(@PathVariable Long idTopico, @RequestBody @Valid DadosRespostaPostagem dados, UriComponentsBuilder uriBuilder){
         var dadosDetalhamentoResposta = respostaService.postar(idTopico, dados);
         var uri = uriBuilder.path("/respostas/{idResposta}").buildAndExpand(dadosDetalhamentoResposta.id()).toUri();
@@ -42,6 +51,7 @@ public class RespostaController {
 
     @PutMapping("/{idResposta}")
     @Transactional
+    @Operation(summary = "Atualizar/Editar Resposta por ID", description = "Atualizar/editar as respostas dos tópicos filtrando das mesmas pelo ID da resposta.")
     public ResponseEntity atualizar(@PathVariable Long idResposta, @RequestBody DadosRespostaAtualizacao dados){
         var dadosDetalhamentoResposta = respostaService.atualizar(idResposta, dados);
         return ResponseEntity.ok(dadosDetalhamentoResposta);
@@ -49,6 +59,7 @@ public class RespostaController {
 
     @DeleteMapping("/{idResposta}")
     @Transactional
+    @Operation(summary = "Excluir Resposta por ID", description = "Excluir uma resposta selecionando o mesmo pelo ID da resposta.")
     public ResponseEntity excluir(@PathVariable Long idResposta){
         respostaService.excluir(idResposta);
         return ResponseEntity.noContent().build();
--- src/main/java/br/com/github/gpagio/api/forumhub/controller/UsuarioController.java
+++ src/main/java/br/com/github/gpagio/api/forumhub/controller/UsuarioController.java
@@ -1,12 +1,18 @@
 package br.com.github.gpagio.api.forumhub.controller;
 
-import br.com.github.gpagio.api.forumhub.domain.usuario.*;
+import br.com.github.gpagio.api.forumhub.domain.usuario.DadosUsuarioAtualizacao;
+import br.com.github.gpagio.api.forumhub.domain.usuario.DadosUsuarioCadastro;
+import br.com.github.gpagio.api.forumhub.domain.usuario.UsuarioRepository;
+import br.com.github.gpagio.api.forumhub.domain.usuario.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +33,10 @@ public class UsuarioController {
     private UsuarioRepository usuarioRepository;
 
     @GetMapping
+    @Operation(summary = "Listar Usuários Cadastrados", description = "Lista todos os usuários cadastrados, tendo a opção de filtrar por usuários ativos. A orderalção padrão é pelo ID do usuário em páginas de 10 itens.")
+    @Parameter(in = ParameterIn.QUERY, description = "Número da página para exibir. Primeira página é 0.", name = "page", content = @Content(schema = @Schema(type = "integer", defaultValue = "0")))
+    @Parameter(in = ParameterIn.QUERY, description = "Quantidade de itens exibidos. Valor padrão é 10.", name = "size", content = @Content(schema = @Schema(type = "integer", defaultValue = "10")))
+    @Parameter(in = ParameterIn.QUERY, description = "Critério de ordenação dos itens. Campo padrão é ID.", name = "sort", content = @Content(schema = @Schema(type = "string", defaultValue = "id")))
     public ResponseEntity listarTodosUsuarios(@Parameter(hidden = true) @PageableDefault(size = 10, sort = "id") Pageable paginacao,
                                               @RequestParam(required = true, name = "ativo") Boolean ativo){
         var page = usuarioService.listar(paginacao, ativo);
@@ -35,6 +45,7 @@ public class UsuarioController {
 
     @PostMapping
     @Transactional
+    @Operation(summary = "Cadastrar Usuário", description = "Realiza o cadastro de um usuário.")
     public ResponseEntity cadastrar(@RequestBody @Valid DadosUsuarioCadastro dados, UriComponentsBuilder uriBuilder){
         var dadosDetalhamentoUsuario = usuarioService.cadastrar(dados);
         var uri = uriBuilder.path("/topicos/{id}").buildAndExpand(dadosDetalhamentoUsuario.id()).toUri();
@@ -43,6 +54,7 @@ public class UsuarioController {
 
     @PutMapping("/{id}")
     @Transactional
+    @Operation(summary = "Atualizar/Editar Usuário por ID", description = "Atualizar/editar as informações dos usuários cadastrados pelo ID do usuário.")
     public ResponseEntity atualizar(@PathVariable Long id, @RequestBody @Valid DadosUsuarioAtualizacao dados){
         var dadosDetalhamentoUsuario = usuarioService.atualizar(id, dados);
         return ResponseEntity.ok(dadosDetalhamentoUsuario);
@@ -50,6 +62,7 @@ public class UsuarioController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @Operation(summary = "Exclusão Logica Usuário por ID", description = "Excluir de forma lógica (ativado/desativado) um usuário pelo ID do mesmo.")
     public ResponseEntity desativar(@PathVariable Long id){
         usuarioService.desativar(id);
         return ResponseEntity.noContent().build();
